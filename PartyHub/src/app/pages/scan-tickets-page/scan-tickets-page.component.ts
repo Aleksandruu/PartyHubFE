61cf5d542c5acc7880422371a370720a38a52517
Gureanu <gureanuraul24@gmail.com>
2023-11-28 18:04:08 +0200

    created empty components and pages

---
+++ PartyHub/src/app/pages/scan-tickets-page/scan-tickets-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scan-tickets-page',
+  templateUrl: './scan-tickets-page.component.html',
+  styleUrls: ['./scan-tickets-page.component.css']
+})
+export class ScanTicketsPageComponent {
+
+}
