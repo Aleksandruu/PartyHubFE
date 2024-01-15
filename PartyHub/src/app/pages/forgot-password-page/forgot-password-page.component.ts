fee07cb1e0bfa7334e750a6bb2554c30b655e7d8
AndraNichifor <andra.nichifor@student.upt.ro>
2024-01-15 16:13:38 +0200

    started forgot password page

--- PartyHub/src/app/pages/forgot-password-page/forgot-password-page.component.ts
+++ PartyHub/src/app/pages/forgot-password-page/forgot-password-page.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PATHS } from 'src/app/constants/paths';
 
 @Component({
   selector: 'app-forgot-password-page',
-  standalone: true,
-  imports: [],
   templateUrl: './forgot-password-page.component.html',
-  styleUrl: './forgot-password-page.component.css'
+  styleUrls: ['./forgot-password-page.component.css'],
 })
-export class ForgotPasswordPageComponent {
+export class ForgotPasswordPageComponent implements OnInit {
+  forgotPasswordForm!: FormGroup;
 
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.forgotPasswordForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  navigateToRegisterPage() {
+    this.router.navigate([PATHS.REGISTER]);
+  }
+
+  navigateToResetPassword() {
+    this.router.navigate([PATHS.FORGOTPASSWORD]);
+  }
 }
