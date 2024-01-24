fa403b947e9ecb9fccebd4d7c17b645fba39fc9a
Gureanu <gureanuraul24@gmail.com>
2024-01-24 18:46:56 +0200

    add edit event

--- PartyHub/src/app/pages/add-edit-page/add-edit-page.component.ts
+++ PartyHub/src/app/pages/add-edit-page/add-edit-page.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-add-edit-page',
+  selector: 'app-add-event',
   templateUrl: './add-edit-page.component.html',
-  styleUrls: ['./add-edit-page.component.css']
+  styleUrls: ['./add-edit-page.component.css'],
 })
 export class AddEditPageComponent {
+  addEventForm = new FormGroup({
+    name: new FormControl(''),
+    location: new FormControl(''),
+    date: new FormControl(''),
+    details: new FormControl(''),
+    price: new FormControl(''),
+    discount: new FormControl(''),
+  });
 
+  onSubmit() {
+    // Logic to handle form submission
+  }
+
+  addMainBanner() {
+    // Logic to handle adding a main banner
+  }
+
+  addSecondaryBanner() {
+    // Logic to handle adding a secondary banner
+  }
 }
