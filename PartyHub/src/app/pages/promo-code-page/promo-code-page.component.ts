eb8be024e7f93a1c75018a1d063495e3067922ac
Gureanu <gureanuraul24@gmail.com>
2024-01-29 14:34:38 +0200

    promo code

--- PartyHub/src/app/pages/promo-code-page/promo-code-page.component.ts
+++ PartyHub/src/app/pages/promo-code-page/promo-code-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-promo-code-page',
   templateUrl: './promo-code-page.component.html',
-  styleUrls: ['./promo-code-page.component.css']
+  styleUrls: ['./promo-code-page.component.css'],
 })
 export class PromoCodePageComponent {
-
+  promoCodeForm = new FormGroup({
+    code: new FormControl(''),
+  });
 }
